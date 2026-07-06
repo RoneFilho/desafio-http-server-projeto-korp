45c51f5a8bb9def6537ff0e8b8e2cde0b9733eab
Rone Filho <ronefilho28@hotmail.com>
2026-07-06 20:54:19 -0300

    http server em GoLang
    Commit inicial do http server funcional com método GET projeto-korp retornano JSON com nome do projeto e horário atual UTC+0 na porta 8080

---
+++ http-server-projeto-korp/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Info struct {
+	Nome    string `json:"nome"`
+	Horario string `json:"horario"`
+}
+
+func getProjetoKorp(c *gin.Context) {
+	horario := Info{
+		Nome:    "Projeto Korp",
+		Horario: time.Now().UTC().Format(time.TimeOnly),
+	}
+	c.IndentedJSON(http.StatusOK, horario)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/projeto-korp", getProjetoKorp)
+
+	router.Run("localhost:8080")
+}
