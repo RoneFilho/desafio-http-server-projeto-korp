0234f96f9450e746216224775e12aab945591472
Rone Filho <ronefilho28@hotmail.com>
2026-07-08 16:37:03 -0300

    Adicionado endpoint de métricas no http server e observabilidade com prometheus grafana
    Adicionado /metrics no http server para o prometheus pegar informações da aplicação. Adicionado prometheus e grafana no arquivo de compose com seus respectivos arquivos de configuração. Adicionado requisição de métricas para o monitoramento do docker. Adicionado arquivo yml de dashboards e datasources para configuração automatica entre grafana e prometheus além de um JSON de um dashboard pronto para visualização de métricas do docker e http server.

--- http-server-projeto-korp/main.go
+++ http-server-projeto-korp/main.go
@@ -2,14 +2,60 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+// Define métricas
+var (
+	HttpRequestTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "api_http_request_total",
+		Help: "Total number of requests processed by the API",
+	}, []string{"path", "status"})
+
+	HttpRequestErrorTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "api_http_request_error_total",
+		Help: "Total number of errors returned by the API",
+	}, []string{"path", "status"})
+)
+
+// Registry customizada (sem métricas padrão Golang)
+var customRegistry = prometheus.NewRegistry()
 
 type Info struct {
 	Nome    string `json:"nome"`
 	Horario string `json:"horario"`
+}
+
+// Registrar métricas com registry customizada
+func init() {
+	customRegistry.MustRegister(HttpRequestTotal, HttpRequestErrorTotal)
+}
+
+// Handler de métricas customizadas com registry customizada
+func PrometheusHandler() gin.HandlerFunc {
+	h := promhttp.HandlerFor(customRegistry, promhttp.HandlerOpts{})
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Middleware para salvar requisições de métricas
+func RequestMetricsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		path := c.Request.URL.Path
+		c.Next()
+		status := c.Writer.Status()
+		if status < 400 {
+			HttpRequestTotal.WithLabelValues(path, strconv.Itoa(status)).Inc()
+		} else {
+			HttpRequestErrorTotal.WithLabelValues(path, strconv.Itoa(status)).Inc()
+		}
+	}
 }
 
 func getProjetoKorp(c *gin.Context) {
@@ -22,6 +68,8 @@ func getProjetoKorp(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
+	router.Use(RequestMetricsMiddleware())
+	router.GET("/metrics", PrometheusHandler())
 	router.GET("/projeto-korp", getProjetoKorp)
 
 	router.Run("0.0.0.0:8080")
